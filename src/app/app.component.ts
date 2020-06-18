4f5ad4614e2ab1d237e7a9842c67df913e0f92f5
Satish Prasad <spd61185@gmail.com>
2020-06-17 23:28:57 -0700

    my-app-ng-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  columnDefs = [
+    {field: 'make' },
+    {field: 'model' },
+    {field: 'price'},
+     {field: 'Test'}
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 ,Test: 'Test1'},
+    { make: 'Ford', model: 'Mondeo', price: 32000 ,Test: 100},
+    { make: 'Porsche', model: 'Boxter', price: 72000,Test:'10test' }
+    
+];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, AgGridModule.withComponents([]) ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
